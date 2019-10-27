f434a478a7f8187c335d8d5e6d03bf5382dba88b
Sergey_Dovadzhyan <dovadzhan@gmail.com>
2019-10-27 12:49:11 +0400

    Upgrade to Spring Boot 2

--- src/main/java/com/epam/mentoring/springboot/controller/ErrorController.java
+++ src/main/java/com/epam/mentoring/springboot/controller/ErrorController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping("/error")
     public String handleError() {
